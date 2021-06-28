1c26490e3f82fadd09eec275c377aaaf7068d2cd
Nabok Serhii <s.nabock@gmail.com>
2021-06-28 23:57:17 +0300

    added script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// import { days, hours, mins, secs } from "./date.js";
-import CountdownTimer from "./timer.js"
-
-const timer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jun 27, 2021'),
-    title: 'До кінця світу залишилось',
-});
-timer.startTimer()
