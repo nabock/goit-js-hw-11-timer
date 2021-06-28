11865dbbc881bae69be150a4c5b48ebea3b89c33
Nabok Serhii <s.nabock@gmail.com>
2021-06-29 00:15:38 +0300

    updated js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ import CountdownTimer from "./timer.js"
 
 const timer = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jun 28, 2021'),
+    targetDate: new Date(),
     title: 'Предел моих возможностей закончится через:',
 });
 timer.startTimer()
+
+// new Date('Jun 28, 2021')
