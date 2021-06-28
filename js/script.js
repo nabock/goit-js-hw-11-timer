16dd0fe11ccfdbc9243d906ec435e3b23cc50276
Nabok Serhii <s.nabock@gmail.com>
2021-06-29 00:23:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ import CountdownTimer from "./timer.js"
 
 const timer = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date(),
+    targetDate: new Date('Jun 31, 2021'),
     title: 'Предел моих возможностей закончится через:',
 });
 timer.startTimer()
 
-// new Date('Jun 28, 2021')
+// targetDate: new Date('Jun 31, 2021'), - нет такой даты, почему не выбивает такую ошибку?
+
+// когда прописываю дату new Date('Jun 32, 2021'), пишет: 
+// NaN Days
+// NaN Hours
+// NaN Minutes
+// NaN Seconds
+
+
+// просто интересно
